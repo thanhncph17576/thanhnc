3db8335b6f307fdd85e2f7a571651a006545d464
thanhncph17576 <thanhncph17576@fpt.edu.vn>
2021-11-17 14:43:33 +0700

    thanhDAO

--- src/ngaynau/Ngaynau.java
+++ src/ngaynau/Ngaynau.java
@@ -17,6 +17,7 @@ public class Ngaynau {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Aaa");
+        System.err.println("kkkkk");
     }
     
 }
